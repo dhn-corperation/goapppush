222a16649176af05f23bf0c412d57b8bf170db69
dohwe0528@gmail.com <dohwe0528@gmail.com>
2025-04-17 10:36:08 +0900

    modified result

--- src/send/result.go
+++ src/send/result.go
@@ -61,9 +61,23 @@ func resultProcess(wg *sync.WaitGroup){
 	var sucIds []string
 	var failIds []string
 
+	var ids2 []string
+
 	var isProc = true
 
-	var groupQuery = "select queueidx, msgidx, step, error_code, resultdate, extra2 from " + cf.Conf.APP_RESPONSE_TABLE + " a where extra3 = '' and step in ('C', 'F')"
+	var groupQuery = `
+	select 
+		a.queueidx,
+		a.msgidx,
+		a.step,
+		a.error_code,
+		a.resultdate,
+		a.extra2,
+		b.profile
+	from ` + cf.Conf.APP_RESPONSE_TABLE + ` a 
+	left join DHN_REQUEST_APP b on a.extra2 = b.msgid 
+	where 
+		a.extra3 = '' and a.step in ('C', 'F')`
 
 	groupRows, err := db.Query(groupQuery)
 	if err != nil {
@@ -78,16 +92,22 @@ func resultProcess(wg *sync.WaitGroup){
 
 	if isProc {
 		for groupRows.Next() {
-			var queueidx, msgidx, step, error_code, resultdate, extra2 sql.NullString
+			var queueidx, msgidx, step, error_code, resultdate, extra2, profile sql.NullString
 			resCode := ""
 
-			groupRows.Scan(&queueidx, &msgidx, &step, &error_code, &resultdate, &extra2)
+			groupRows.Scan(&queueidx, &msgidx, &step, &error_code, &resultdate, &extra2, &profile)
 
-			if s.EqualFold(step.String, "C") {
-				sucIds = append(sucIds, extra2.String)
-			} else if (s.EqualFold(step.String, "F")){
-				failIds = append(failIds, extra2.String)
+			if s.EqualFold(step.String, "C") && len(profile.String) > 0 {
+				cf.Stdlog.Println(1)
+			} else if (s.EqualFold(step.String, "F") && len(profile.String) > 0){
 				resCode = error_code.String
+				cf.Stdlog.Println(2)
+			} else if s.EqualFold(step.String, "C") && len(profile.String) <= 0 {
+				resCode = "0000"
+				cf.Stdlog.Println(3)
+			} else if (s.EqualFold(step.String, "F") && len(profile.String) <= 0){
+				resCode = error_code.String
+				cf.Stdlog.Println(4)
 			}
 			db.Exec("update DHN_REQUEST_APP set message_type = 'at', remark4 = '" + resultdate.String + "', remark5 = '" + resCode + "' where msgid = '" + extra2.String + "'")
 			db.Exec("update " + cf.Conf.APP_RESPONSE_TABLE + " set extra3 = 'Y' where queueidx = '" + queueidx.String + "'")
@@ -102,6 +122,11 @@ func resultProcess(wg *sync.WaitGroup){
 				failIds = []string{}
 			}
 
+			if len(ids2) >= 500 {
+				insRes2(ids2)
+				ids2 = []string{}
+			}
+
 		}
 
 		if len(sucIds) > 0 {
@@ -110,6 +135,10 @@ func resultProcess(wg *sync.WaitGroup){
 
 		if len(failIds) > 0 {
 			insAt(failIds)
+		}
+
+		if len(ids2) > 0 {
+			insRes2(ids2)
 		}
 	}
 }
@@ -232,6 +261,108 @@ func insRes(ids []string){
 	dbpool.DB.Exec(sql)
 }
 
+func insRes2(ids []string){
+	msgids := "'" + s.Join(ids, "', '")	+ "'"
+
+	var resdt = time.Now()
+	var resdtstr = fmt.Sprintf("%4d-%02d-%02d %02d:%02d:%02d", resdt.Year(), resdt.Month(), resdt.Day(), resdt.Hour(), resdt.Minute(), resdt.Second())
+
+	sql := `
+		insert into DHN_RESULT(
+			msgid,
+			userid,
+			ad_flag,
+			button1,
+			button2,
+			button3,
+			button4,
+			button5,
+			code,
+			image_link,
+			image_url,
+			message_type,
+			msg,
+			msg_sms,
+			only_sms,
+			p_com,
+			p_invoice,
+			phn,
+			profile,
+			reg_dt,
+			remark1,
+			remark2,
+			remark3,
+			remark4,
+			remark5,
+			res_dt,
+			reserve_dt,
+			s_code,
+			result,
+			sms_kind,
+			sms_lms_tit,
+			sms_sender,
+			sync,
+			tmpl_id,
+			wide,
+			send_group,
+			supplement,
+			price,
+			currency_type,
+			title,
+			header,
+			carousel,
+			attachments
+		)
+		select 
+			msgid,
+			userid,
+			ad_flag,
+			button1,
+			button2,
+			button3,
+			button4,
+			button5,
+			remark5,
+			image_link,
+			image_url,
+			'ap',
+			msg,
+			msg_sms,
+			only_sms,
+			p_com,
+			p_invoice,
+			phn,
+			profile,
+			reg_dt,
+			remark1,
+			'` + resdtstr + `',
+			remark3,
+			remark4,
+			remark5,
+			now(),
+			reserve_dt,
+			s_code,
+			'Y',
+			sms_kind,
+			sms_lms_tit,
+			sms_sender,
+			'N',
+			tmpl_id,
+			wide,
+			null,
+			supplement,
+			price,
+			currency_type,
+			title,
+			header,
+			carousel,
+			attachments
+		from DHN_REQUEST_APP
+		where msgid in (` + msgids + `)
+	`
+
+	dbpool.DB.Exec(sql)
+}
 
 
 
